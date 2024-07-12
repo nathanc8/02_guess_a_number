49150c4aa09ca7965081ed1924349f03dbb27845
Nathan Cazard <cazard.nathan@gmail.com>
2024-07-12 23:47:54 +0200

    FIN DU TYPESCRIPT YOUPIIIII

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
+var proposed_number;
 var number_player1 = function () {
-    var proposed_number = parseInt(prompt("Player 1, give player 2 a number to guess"));
+    proposed_number = parseInt(prompt('Player 1, give player 2 a number to guess'));
     var error_message;
-    while (isNaN(proposed_number) ||
-        proposed_number < 0 ||
-        proposed_number > 50) {
+    while (isNaN(proposed_number) || proposed_number < 0 || proposed_number > 50) {
         if (isNaN(proposed_number)) {
-            error_message = "This is not a number, please try again";
+            error_message = 'This is not a number, please try again';
         }
         else {
             error_message = "The number isn't between 0 and 50";
         }
         proposed_number = parseInt(prompt(error_message));
     }
-    console.log(proposed_number);
+    console.log('proposed number is : ' + proposed_number, typeof proposed_number);
     return proposed_number;
 };
-//number_player1();
 var number_player2 = function () {
-    var given_number = parseInt(prompt("Player 2, try to guess  the number !"));
-    while (isNaN(given_number))
-        given_number = parseInt(prompt("This is not a number, please try again."));
+    var input_number = document.getElementById('guess_number');
+    var given_number = parseInt(input_number.value);
+    input_number.value = '';
+    console.log('given_number is ' + given_number, typeof given_number);
+    if (isNaN(given_number)) {
+        given_number = parseInt(prompt('This is not a number, please try again.'));
+    }
     return given_number;
 };
-var did_i_win = function (given_number) {
-    var win = false;
-    while (!win) {
-        if (given_number === 22) {
-            alert("Congrats ! You guessed the number");
-            win = true;
-        }
-        else if (given_number < 22) {
-            alert("Higher !");
-        }
-        else {
-            alert("Lower !");
-        }
+var did_i_win = function (given_number, correct_number) {
+    if (given_number === correct_number) {
+        return true;
     }
-    return win;
+    else if (given_number < correct_number) {
+        alert('Higher !');
+    }
+    else {
+        alert('Lower !');
+    }
+    return false;
 };
-var gameplay = function () {
+var guess = 0;
+var gameplay = function (event) {
     var win = false;
-    while (!win) {
+    if (!win) {
         var guessing_number = number_player2();
-        win = did_i_win(guessing_number);
+        console.log('guessing_number is : ' + guessing_number, typeof guessing_number);
+        event.preventDefault();
+        win = did_i_win(guessing_number, proposed_number);
+        guess += 1;
+        console.log(guess);
+        document.getElementById('guesses').innerText = "You did ".concat(guess, " guesses");
+        if (win) {
+            document.getElementById('game').className = 'invisible';
+            document.getElementById('congratulations').className = 'visible';
+        }
     }
 };
-gameplay();
--- script.ts
+++ script.ts
@@ -1,58 +1,55 @@
-const number_player1: Function = () => {
-    let proposed_number: number = parseInt(
-        prompt("Player 1, give player 2 a number to guess")!
-    );
+let proposed_number: number;
+const number_player1 = (): number => {
+    proposed_number = parseInt(prompt('Player 1, give player 2 a number to guess')!);
     let error_message: string;
-    while (
-        isNaN(proposed_number) ||
-        proposed_number < 0 ||
-        proposed_number > 50
-    ) {
+    while (isNaN(proposed_number) || proposed_number < 0 || proposed_number > 50) {
         if (isNaN(proposed_number)) {
-            error_message = "This is not a number, please try again";
+            error_message = 'This is not a number, please try again';
         } else {
             error_message = "The number isn't between 0 and 50";
         }
         proposed_number = parseInt(prompt(error_message)!);
     }
-    console.log(proposed_number);
+    console.log('proposed number is : ' + proposed_number, typeof proposed_number);
     return proposed_number;
 };
 
-//number_player1();
-
-const number_player2: Function = (): number => {
-    let given_number: number = parseInt(
-        prompt("Player 2, try to guess  the number !")!
-    );
-    while (isNaN(given_number))
-        given_number = parseInt(
-            prompt("This is not a number, please try again.")!
-        );
+const number_player2 = (): number => {
+    let input_number = document.getElementById('guess_number') as HTMLInputElement;
+    let given_number = parseInt(input_number.value);
+    input_number.value = '';
+    console.log('given_number is ' + given_number, typeof given_number);
+    if (isNaN(given_number)) {
+        given_number = parseInt(prompt('This is not a number, please try again.')!);
+    }
     return given_number;
 };
 
-const did_i_win = (given_number: number): boolean => {
+const did_i_win = (given_number: number, correct_number: number): boolean => {
+    if (given_number === correct_number) {
+        return true;
+    } else if (given_number < correct_number) {
+        alert('Higher !');
+    } else {
+        alert('Lower !');
+    }
+    return false;
+};
+
+let guess: number = 0;
+const gameplay = (event): void => {
     let win: boolean = false;
-    while (!win) {
-        if (given_number === 22) {
-            alert("Congrats ! You guessed the number");
-            win = true;
-        } else if (given_number < 22) {
-            alert("Higher !");
-        } else {
-            alert("Lower !");
+    if (!win) {
+        let guessing_number = number_player2();
+        console.log('guessing_number is : ' + guessing_number, typeof guessing_number);
+        event.preventDefault();
+        win = did_i_win(guessing_number, proposed_number);
+        guess += 1;
+        console.log(guess);
+        document.getElementById('guesses')!.innerText = `You did ${guess} guesses`;
+        if (win) {
+            document.getElementById('game')!.className = 'invisible';
+            document.getElementById('congratulations')!.className = 'visible';
         }
     }
-    return win;
 };
-
-const gameplay: Function = (): void => {
-    let win: boolean = false;
-    while (!win) {
-        const guessing_number = number_player2();
-        win = did_i_win(guessing_number);
-    }
-};
-
-gameplay();
